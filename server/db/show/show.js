207cd9392f6e522b62f996a19f0ac409d099eb0a
Mohammed kasem <mohamedtarbosh1@gmail.com>
2017-12-11 12:42:24 +0200

    style/

--- server/db/show/show.js
+++ server/db/show/show.js
@@ -4,8 +4,8 @@ let mongoose = require('mongoose'),
 
 let show = new Schema({
     title: {type: String, require: true, default: "new show", unique: true},
-    img: {type: String, default: "/assets/imgs/1.jpg"},
-    imgCap : {type: String, default: "/assets/imgs/1.jpg"},
+    img: {type: String},
+    imgCap : {type: String},
     // episodesNum: {type: Num},
     aired: String,
     type: String,          
